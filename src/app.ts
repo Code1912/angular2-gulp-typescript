0cf1737fb4609be4f2706ae6ac4ea6d8e8142dda
Code1912 <code1912@163.com>
2016-08-03 00:31:34 +0800

    modify typescript error tip

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 /**
  * Created by Code1912 on 2016/8/1.
  */
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from '../src/app/app.component';
 bootstrap(AppComponent);
