37813134cfa25e44cd3e782a88f0263bf60fbb9c
Code1912 <code1912@163.com>
2016-08-03 21:00:16 +0800

    modify gitignore

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,5 @@
  */
 /// <reference path="../node_modules/angular2/typings/browser.d.ts" />
 import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from '../src/app/app.component';
+import {AppComponent} from './app/app.component';
 bootstrap(AppComponent);
